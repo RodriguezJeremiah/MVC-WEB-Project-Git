3d425138045f98d478068909e498d4f923fcd521
Gerald <g.delatorre@bpsolutions.biz>
2024-01-31 11:18:43 +0800

    Inital commit: added MVC project

---
+++ MVC WEB Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_WEB_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
